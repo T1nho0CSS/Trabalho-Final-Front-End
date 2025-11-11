558e2c01cd6863cc2b00b8ca15642ed1cfc1b1c5
Wellington <wwelingtonjrsccp@gmail.com>
2025-11-10 22:35:44 -0300

    Add files via upload

---
+++ WLLMOTORS/suzuki/suzuki.js
@@ -0,0 +1,47 @@
+function atualizarValor() {
+    const moto = document.getElementById("moto").value;
+    const valorInput= document.getElementById("valor");
+
+    if ( moto === "hayabusa") valorInput.value = 124500
+    else if ( moto === "gsx8r") valorInput.value = 35072
+    else valorInput.value = ""
+
+}
+
+function calcular() {
+    const valor = parseFloat(document.getElementById("valor").value);
+    const metodo = document.getElementById("metodo").value;
+    const resultado = document.getElementById("resultado");
+
+    if (!valor || !metodo) {
+        resultado.textContent = "Por favor, insira o valor e selecione uma método de pagamento";
+        resultado.style.color = "red";
+        return;
+    }
+
+    let mensagem = ""
+    let total = valor
+
+    switch (metodo) {
+        case "credito":
+            mensagem = `Pagamento no Cartão de Crédito em até 12x sem juros de R$ ${(valor / 12).toFixed(2)} por mês.`;
+            break;
+        case "debito":
+            total = valor * 0.9
+            mensagem = `Pagamento no Cartão de Débito à vista , ganha um desconto de 9%. O valor então será R$ ${total.toFixed(2)} por mês.`;
+            break;
+        case "consorcio":
+            mensagem = `Consórcio da Suzuki disponivel por pagamento ao Mês de no mínimo R$ 200,00.`;
+            break;
+        case "pix":
+            total = valor * 0.93
+            mensagem = `Pagamento via Pix à vista terá um desconto de 7%. Resultando em um valor de R$ ${total.toFixed(2)}.`;
+            break;
+        default:
+            mensagem = "Selecione um método válido"
+
+    }
+
+    resultado.textContent = mensagem;
+    resultado.style.color = "green";
+}
